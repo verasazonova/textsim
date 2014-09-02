96531cc851f454e96832fed3633e48acfe5257fe
Vera Sazonova <verasazonova@gmail.com>
2014-09-02 10:41:31 -0400

    Created a test script to see if sklearn can be run in parallel on sharcnet

--- test_sharcnet_parallel.py
+++ test_sharcnet_parallel.py
@@ -2,22 +2,21 @@ __author__ = 'verasazonova'
 
 import numpy as np
 from sklearn import cross_validation
-from sklearn import datasets
 from sklearn import svm
 from sklearn.utils import shuffle
-
+from sklearn.datasets import make_classification
 
 def __main__():
-    iris = datasets.load_iris()
-    print iris.data.shape, iris.target.shape
 
+    x, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
+                           random_state=1)
     n_trials = 10
     n_cv = 5
     clf = svm.SVC(kernel='linear', C=1)
     scores = np.zeros((n_trials * n_cv))
 #    scores = np.empty([n_trials * n_cv])
     for n in range(n_trials):
-        x_shuffled, y_shuffled = shuffle(iris.data, iris.target, random_state=n)
+        x_shuffled, y_shuffled = shuffle(x, y, random_state=n)
         skf = cross_validation.StratifiedKFold(y_shuffled, n_folds=n_cv)  # random_state=n, shuffle=True)
         scores[n * n_cv:(n + 1) * n_cv] = cross_validation.cross_val_score(clf, x_shuffled, y_shuffled, cv=skf,
                                                                            scoring='roc_auc',
